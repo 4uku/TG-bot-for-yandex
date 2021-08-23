d1c2566e5544e71e3348ce059c089c5753e078ac
4uku <demonekr@yandex.ru>
2021-08-23 14:11:16 +0300

    add exception, get method

--- homework.py
+++ homework.py
@@ -14,6 +14,8 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
@@ -27,20 +29,23 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
+    homework_name = homework.get('homework_name')
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
+    elif homework.get('status') == 'reviewing':
+        verdict = 'Работа взята в ревью'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homeworks(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(URL, headers=HEADERS, params=payload)
+        return homework_statuses.json()
+    except Exception as e:
+        return e
 
 
 def send_message(message):
@@ -54,11 +59,18 @@ def main():
     while True:
         try:
             homeworks = get_homeworks(current_timestamp)
-            if len(homeworks['homeworks']) > 1:
-                last_homework = homeworks['homeworks'][0]
-                text_message = parse_homework_status(last_homework)
-                logger.info('Сообщение отправлено.')
-                send_message(text_message)
+            current_timestamp = int(time.time())
+            if type(homeworks) == Exception:
+                logger.error(homeworks, exc_info=True)
+                send_message("Не удалось получить данные от Яндекса")
+            else:
+                if homeworks.get('current_date') is not None:
+                    current_timestamp = homeworks.get('current_date')
+                if len(homeworks.get('homeworks')) > 0:
+                    last_homework = homeworks.get('homeworks')[0]
+                    text_message = parse_homework_status(last_homework)
+                    logger.info('Сообщение отправлено.')
+                    send_message(text_message)
             time.sleep(5 * 60)
         except Exception as e:
             logger.error(e, exc_info=True)
